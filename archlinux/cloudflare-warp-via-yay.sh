b6a45b3942c6c0e35b6ee5c9b65f47a465dc5b3c
gordonlin2011 <64232078+gordonlin2011@users.noreply.github.com>
2024-10-27 12:27:20 -0400

    Update and rename cloudflare-warp-yay.sh to cloudflare-warp-via-yay.sh

--- archlinux/cloudflare-warp-via-yay.sh
+++ archlinux/cloudflare-warp-via-yay.sh
@@ -1,7 +1,3 @@
-sudo pacman -S --needed git base-devel
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
 yay -S cloudflare-warp-bin
 sudo systemctl start warp-svc
 sudo systemctl enable warp-svc
